ab9b863aff52439f93cda7a29a387c98e94cf451
Steven Hollidge <stevenhollidge@hotmail.com>
2013-03-29 11:59:11 +0100

    initial commit

---
+++ WrappingListbox/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+
+namespace WrappingListbox 
+{
+    using System.Windows;
+    using System.Windows.Controls;
+
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            listbox1.ItemsSource = Enumerable.Range(1, 100);
+        }
+
+        public void ListBoxItemSelected(object sender, RoutedEventArgs e)
+        {
+            Panel.SetZIndex((UIElement)sender, 999);
+        }
+    }
+}
